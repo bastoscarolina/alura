4d5e715c8b4ff6616674c8c79af413e249b9b31b
Carolina <almeida.carolinabastos@gmail.com>
2022-11-11 04:45:16 -0300

    implementacao do merge sort para estudo de algoritmos

--- js/js-algoritmos/algorirmos.js
+++ js/js-algoritmos/algorirmos.js
@@ -1,3 +1,5 @@
+const {edFolha, edGalho} = require('./arraysBase') 
+const listaLivros = require('./listaLivros')
 const livros = [
     {
         nome: "Javascript",
@@ -81,4 +83,76 @@ const organizaListaInserindoElemento = (arrProdutos) =>{
     console.log(arrProdutos)
 }
 
-organizaListaInserindoElemento(livros)
+// organizaListaInserindoElemento(livros)
+
+const juntaListas = (lista1,lista2) =>{
+    let listaFinal = []
+    let posicaoAtualLista1 = 0
+    let posicaoAtualLista2 = 0
+    let atual = 0
+    
+    while(posicaoAtualLista1 < lista1.length && posicaoAtualLista2 < lista2.length){
+        let produtoAtualLista1 = lista1[posicaoAtualLista1]
+        let produtoAtualLista2 = lista2[posicaoAtualLista2]
+ 
+        if(produtoAtualLista1.preco < produtoAtualLista2.preco){
+            listaFinal[atual] = produtoAtualLista1
+            posicaoAtualLista1++
+        }else {
+            listaFinal[atual] = produtoAtualLista2
+            posicaoAtualLista2++   
+        }
+        atual++
+    }
+    while(posicaoAtualLista1< lista1.length){
+        listaFinal[atual] = lista1[posicaoAtualLista1]
+        posicaoAtualLista1++
+        atual++
+    }
+    while(posicaoAtualLista2< lista2.length){
+        listaFinal[atual] = lista2[posicaoAtualLista2]
+        posicaoAtualLista2++
+        atual++
+    }
+
+     return listaFinal
+}
+
+// console.log(juntaListas(edGalho, edFolha))
+
+//Merge Sort
+const mergeSort = (arr)=>{
+    if(arr.length>1){
+        const meio = Math.floor(arr.length/2)
+        const parte1 = mergeSort(arr.slice(0,meio))
+        const parte2 = mergeSort(arr.slice(meio, arr.length))
+        arr = ordena(parte1, parte2)
+    }
+
+    return arr
+}
+
+const ordena = (parte1, parte2)=>{
+    let posicaoAtualParte1 = 0
+    let posicaoAtualParte2 = 0
+    const resultado = []
+
+    while(posicaoAtualParte1< parte1.length && posicaoAtualParte2<parte2.length){
+        let produtoAtualParte1 = parte1[posicaoAtualParte1]
+        let produtoAtualParte2 = parte2[posicaoAtualParte2]
+
+        if(produtoAtualParte1.preco < produtoAtualParte2.preco){
+            resultado.push(produtoAtualParte1)
+            posicaoAtualParte1++
+        }else{
+            resultado.push(produtoAtualParte2)
+            posicaoAtualParte2++
+        }
+    }
+
+    return resultado.concat(posicaoAtualParte1<parte1.length 
+        ? parte1.slice(posicaoAtualParte1) 
+        : parte2.slice(posicaoAtualParte2)
+        )
+}
+console.log(mergeSort(listaLivros))
